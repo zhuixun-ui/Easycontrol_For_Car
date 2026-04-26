a3013985e88ae82e27e38f2bf3d7301392f1ed15
zhuixun-ui <1161973794@qq.com>
2026-04-26 18:05:51 +0800

    Update Client.java

--- easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/client/Client.java
+++ easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/client/Client.java
@@ -551,15 +551,6 @@ public class Client {
   }
 
 
-private String getSystemProperty(String key) {
-    try {
-        Class<?> clazz = Class.forName("android.os.SystemProperties");
-        java.lang.reflect.Method get = clazz.getMethod("get", String.class);
-        return (String) get.invoke(null, key);
-    } catch (Exception e) {
-        return null;
-    }
-}
   
 private void startCameraMonitoring() {
     if (!AppData.setting.getMiniRecoverOnTimeout()) return;
